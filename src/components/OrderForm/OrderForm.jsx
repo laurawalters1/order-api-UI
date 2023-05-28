180658683a9ece51f87ec6d60421e84d680d9424
Laura Walters <laura.walters1098@yahoo.com>
2023-05-28 16:34:24 +0100

    response data is logging

--- src/components/OrderForm/OrderForm.jsx
+++ src/components/OrderForm/OrderForm.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 
 export default function OrderForm() {
+    const data = {
+        count: 750
+    }
+    const getPackNumber = async (event) => {
+        event.preventDefault();
+        let response = await fetch("https://bqst6iv3sj.eu-west-1.awsapprunner.com/place-order", {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers : { "Content-Type": "application/json", 'Access-Control-Allow-Origin': '*' }
+            
+        });
+        response.json().then(data=>{
+            console.log(data)
+        })
+        // console.log(json.json())
+      }
   return (
-    <form action="" className='d-flex flex-column w-25 align-items-center'>
+    <form action="" onSubmit={getPackNumber} className='d-flex flex-column w-25 align-items-center'>
         <label htmlFor="orderNumber">How many items would you like to order?</label>
         <input type="number" className='w-100' />
+        <button className='btn btn-light' type='submit'>Submit</button>
     </form>
   )
 }
