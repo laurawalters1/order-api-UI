c6dd4db6d50d6ae0a8fa8a6088388dca01f0d19b
Laura Walters <laura.walters1098@yahoo.com>
2023-05-28 16:44:06 +0100

    request is responsive to suer input

--- src/components/OrderForm/OrderForm.jsx
+++ src/components/OrderForm/OrderForm.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function OrderForm() {
-    const data = {
-        count: 750
-    }
+    const [body, setBody] = useState({ count: 0});
+
+    const handleUserInput = (event) => {
+        const { name, value } = event.target;
+
+        const count = parseInt(value)
+  
+        setBody({[name]: count });
+      };
     const getPackNumber = async (event) => {
         event.preventDefault();
+        console.log(body)
         let response = await fetch("https://bqst6iv3sj.eu-west-1.awsapprunner.com/place-order", {
             method: "POST",
-            body: JSON.stringify(data),
+            body: JSON.stringify(body),
             headers : { "Content-Type": "application/json", 'Access-Control-Allow-Origin': '*' }
             
         });
@@ -20,7 +27,7 @@ export default function OrderForm() {
   return (
     <form action="" onSubmit={getPackNumber} className='d-flex flex-column w-25 align-items-center'>
         <label htmlFor="orderNumber">How many items would you like to order?</label>
-        <input type="number" className='w-100' />
+        <input type="number" onChange={handleUserInput} name='count' className='w-100' />
         <button className='btn btn-light' type='submit'>Submit</button>
     </form>
   )
